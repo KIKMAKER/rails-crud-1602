888cf9da397d423b60eeab2650c1245b373b099b
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2024-05-18 09:38:11 +0200

    added address to restaurants and set default values for rating and address

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_18_072355) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_18_072744) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.integer "rating"
+    t.integer "rating", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address", default: "N/A"
   end
 
 end
